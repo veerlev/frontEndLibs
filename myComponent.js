cbc8a20bf79c04999aa3cbd23f705dc4fe72827b
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-10 20:12:46 +0300

    Add child component

--- myComponent.js
+++ myComponent.js
@@ -12,3 +12,28 @@ class MyComponent extends React.Component {
     // change code above this line
   }
 };
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        { /* change code below this line */ }
+        <ChildComponent />
+
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
