79f24973bf165fc1a776f02c718a886990143b63
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-14 23:20:25 +0300

    Create willMount.js

---
+++ willMount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+    <div>{console.log("now it logs")}</div>
+    // change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
