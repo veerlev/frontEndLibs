e1c4f2f82d369d5cece470ca79b0bc8afa91410f
veerlev <47982314+veerlev@users.noreply.github.com>
2019-06-26 14:55:26 +0300

    Create uniqueSiblingKey.js

---
+++ uniqueSiblingKey.js
@@ -0,0 +1,23 @@
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((framework, i) =>{
+    return <li key={i}>{framework}</li>
+  }); // change code here
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
