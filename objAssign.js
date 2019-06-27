f9575e39b176fb25bddf36ec5235520e55cf14db
veerlev <47982314+veerlev@users.noreply.github.com>
2019-06-27 14:42:20 +0300

    Create objAssign.js

---
+++ objAssign.js
@@ -0,0 +1,29 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+      return Object.assign({}, {
+        user: state.user,
+        status: 'online',
+        friends: state.friends,
+        community: state.community
+        });
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
