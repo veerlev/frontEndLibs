8933faa1a69e10607cbfe23daf658721552a8dad
veerlev <47982314+veerlev@users.noreply.github.com>
2019-09-02 17:15:51 +0100

    Getting Started with React Redux

--- RR_Start.js
+++ RR_Start.js
@@ -1,6 +1,11 @@
 class DisplayMessages extends React.Component {
   // change code below this line
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      input : "",
+      messages : []
+  }
   // change code above this line
   render() {
     return <div />
